b314d02a4210ad9d8747cfd6062c0c66674be10d
Chris Petta <cpetta@oaisd.org>
2022-03-20 14:34:28 -0400

    Update char representation of Board
    Character representation of board should be derived from the int representation rather than being it's own separate data structure.

--- ticTacToe/Board.cs
+++ ticTacToe/Board.cs
@@ -16,11 +16,38 @@ namespace ticTacToe
 			{ -1, -1 ,-1 }
 		};
 
-		private string[,] textButtonValues = new string[3, 3]
+		public int[,] getBoardInt()
+        {
+			return board;
+        }
+
+		public char[,] getBoardChar()
 		{
-			{ " ", " ", " " },
-			{ " ", " ", " " },
-			{ " ", " ", " " }
-		};
+			char[,] charBoard = new char[3, 3];
+
+			for(int i = 0; i < 3; i++)
+            {
+				for (int j = 0; j < 3 ; j++)
+                {
+					charBoard[i, j] = getPosChar(i, j);
+                }
+            }
+			return charBoard;
+		}
+
+		public int getPosInt(int x,int y)
+        {
+			return board[x,y];
+        }
+
+		public char getPosChar(int x, int y)
+        {
+			if (board[x, y] == 0)
+				return '0';
+			else if (board[x, y] == 1)
+				return 'X';
+			else
+				return ' ';
+		}
     }
 }
